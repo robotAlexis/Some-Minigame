46d16734390b54ffe18aa9127921a0a325c55229
Alexis <alexis.karpov@mail.ru>
2026-09-06 22:08:51 +0500

    Update the cursor for Web

--- src/some_field.gd
+++ src/some_field.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var label: Label = $Label
+@onready var cursor: Polygon2D = $Polygon2D
 const some_cell_scene = preload("res://some_cell.tscn")
 enum Direction {
 	TOP,
@@ -19,13 +20,11 @@ var field_init_data: Array[bool] = [
 	true,	true,	true,	true,	true,	true,
 	true,	true,	true,	true,	true,	true
 ]
-
 var cell_size: int = 64
 var current_row: int = 0
 var current_col: int = 0
 var final_row: int = 2
 var filan_col: int = 3
-var prev_mouse_pos: Vector2
 
 class Cell:
 	var enabled: bool
@@ -35,21 +34,26 @@ class Cell:
 var field: Array[Cell] = []
 
 var started: bool = false
+var prev_pos: Vector2
+var curr_pos: Vector2
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	init_field()
 
-func _physics_process(delta: float) -> void:
-	if(started):
+func _input(event: InputEvent):
+	if (started && event is InputEventMouseMotion):
+		curr_pos += event.relative
 		if(check_mouse_movement()):
 			started = false
-		
-	if(Input.is_key_pressed(KEY_R)):
+		cursor.position = curr_pos
+	
+	if(event is InputEventKey && event.is_pressed() && !event.is_echo() && event.keycode  == KEY_R):
 		lets_get_it_restarted()
-	if(Input.is_key_pressed(KEY_ESCAPE)):
+	if(event is InputEventKey && event.is_pressed() && !event.is_echo() && event.keycode  == KEY_ESCAPE):
 		lets_get_it_stopped()
+
 
 func init_field():
 	field.resize(height * width)
@@ -71,8 +75,9 @@ func lets_get_it_restarted():
 	field[0].some_cell.set_current()
 	current_row = 0
 	current_col = 0
-	prev_mouse_pos = Vector2(cell_size * .5, cell_size * .5)
-	DisplayServer.warp_mouse(prev_mouse_pos)
+	prev_pos = Vector2(cell_size * .5, cell_size * .5)
+	curr_pos = prev_pos
+	cursor.position = curr_pos
 	started = true
 
 func lets_get_it_stopped():
@@ -88,29 +93,28 @@ func check_mouse_movement() -> bool:
 	var bottom_limit: int = current_row * cell_size + cell_size
 	var left_limit: int = current_col * cell_size
 	var right_limit: int = current_col * cell_size + cell_size
-	var new_mouse_pos: Vector2 = get_global_mouse_position()
-	var mouse_vector: Vector2 = new_mouse_pos - prev_mouse_pos
+	var cursor_vector: Vector2 = curr_pos - prev_pos
 	
 	# Проверяем выходы за пределы текущей клетки
-	if(new_mouse_pos.y < top_limit || new_mouse_pos.y > bottom_limit || new_mouse_pos.x < left_limit || new_mouse_pos.x > right_limit):
+	if(curr_pos.y < top_limit || curr_pos.y > bottom_limit || curr_pos.x < left_limit || curr_pos.x > right_limit):
 		# Проверяем направление выхода по порядку: верх, лево, низ, право
 		# Векторы от предыдущего положения до углов клетки
-		var v_to_top_right: Vector2 = Vector2(right_limit - prev_mouse_pos.x, top_limit - prev_mouse_pos.y)
-		var v_to_top_left: Vector2 = Vector2(left_limit - prev_mouse_pos.x, top_limit - prev_mouse_pos.y)
-		var v_to_bottom_right: Vector2 = Vector2(right_limit - prev_mouse_pos.x, bottom_limit - prev_mouse_pos.y)
-		var v_to_bottom_left: Vector2 = Vector2(left_limit - prev_mouse_pos.x, bottom_limit - prev_mouse_pos.y)
+		var v_to_top_right: Vector2 = Vector2(right_limit - prev_pos.x, top_limit - prev_pos.y)
+		var v_to_top_left: Vector2 = Vector2(left_limit - prev_pos.x, top_limit - prev_pos.y)
+		var v_to_bottom_right: Vector2 = Vector2(right_limit - prev_pos.x, bottom_limit - prev_pos.y)
+		var v_to_bottom_left: Vector2 = Vector2(left_limit - prev_pos.x, bottom_limit - prev_pos.y)
 		var direction: Direction = Direction.NONE
 		# Проверка на выход вверх
-		if(new_mouse_pos.y < top_limit && v_to_top_right.orthogonal().dot(mouse_vector) > 0 && v_to_top_left.orthogonal().dot(mouse_vector) < 0):
+		if(curr_pos.y < top_limit && v_to_top_right.orthogonal().dot(cursor_vector) > 0 && v_to_top_left.orthogonal().dot(cursor_vector) < 0):
 			direction = Direction.TOP
 		# Проверка на выход влево
-		elif(new_mouse_pos.x < left_limit && v_to_top_left.orthogonal().dot(mouse_vector) > 0 && v_to_bottom_left.orthogonal().dot(mouse_vector) < 0):
+		elif(curr_pos.x < left_limit && v_to_top_left.orthogonal().dot(cursor_vector) > 0 && v_to_bottom_left.orthogonal().dot(cursor_vector) < 0):
 			direction = Direction.LEFT
 		# Проверка на выход вниз
-		elif(new_mouse_pos.y > bottom_limit && v_to_bottom_left.orthogonal().dot(mouse_vector) > 0 && v_to_bottom_right.orthogonal().dot(mouse_vector) < 0):
+		elif(curr_pos.y > bottom_limit && v_to_bottom_left.orthogonal().dot(cursor_vector) > 0 && v_to_bottom_right.orthogonal().dot(cursor_vector) < 0):
 			direction = Direction.BOTTOM
 		# Проверка на выход вправо
-		elif(new_mouse_pos.x > right_limit && v_to_bottom_right.orthogonal().dot(mouse_vector) > 0 && v_to_top_right.orthogonal().dot(mouse_vector) < 0):
+		elif(curr_pos.x > right_limit && v_to_bottom_right.orthogonal().dot(cursor_vector) > 0 && v_to_top_right.orthogonal().dot(cursor_vector) < 0):
 			direction = Direction.RIGHT
 		
 		# Индексы новой клетки
@@ -125,23 +129,23 @@ func check_mouse_movement() -> bool:
 		var bound_position: Vector2
 		if(direction == Direction.TOP || direction == Direction.BOTTOM):
 			bound_position.y = top_limit if direction == Direction.TOP else bottom_limit
-			bound_position.x = prev_mouse_pos.x + mouse_vector.x * (bound_position.y - prev_mouse_pos.y) / mouse_vector.y
+			bound_position.x = prev_pos.x + cursor_vector.x * (bound_position.y - prev_pos.y) / cursor_vector.y
 		if(direction == Direction.LEFT || direction == Direction.RIGHT):
 			bound_position.x = left_limit if direction == Direction.LEFT else right_limit
-			bound_position.y = prev_mouse_pos.y + mouse_vector.y * (bound_position.x - prev_mouse_pos.x) / mouse_vector.x
+			bound_position.y = prev_pos.y + cursor_vector.y * (bound_position.x - prev_pos.x) / cursor_vector.x
 		
 		# Если выход недоступен, то отодвигаем курсор от границ текущей клетки на пару пикселе, обновляем значения и заканчиваем проверку
 		if(!is_ok):
 			bound_position.x = clamp(bound_position.x, left_limit + 2, right_limit - 2)
 			bound_position.y = clamp(bound_position.y, top_limit + 2, bottom_limit - 2)
-			prev_mouse_pos = bound_position
-			DisplayServer.warp_mouse(prev_mouse_pos)
+			prev_pos = bound_position
+			curr_pos = prev_pos
 			return false
 		# Если выход доступен, то отодвигаем курсор на пару пикселей от границ новой клетки, обновляем значения и делаем новую проверку
 		else:
 			bound_position.x = clamp(bound_position.x, left_limit + 2, right_limit - 2)
 			bound_position.y = clamp(bound_position.y, top_limit + 2, bottom_limit - 2)
-			prev_mouse_pos = bound_position
+			prev_pos = bound_position
 			# Блокируем текущую клетку
 			field[current_row * width + current_col].enabled = false
 			field[current_row * width + current_col].some_cell.set_active(false)
